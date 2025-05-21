4f5d3c36693e93486621869876f824ea48817cdd
Teja <saitejanomula143@gmail.com>
2025-05-21 22:00:20 +0530

    Revert "mailto was updated"
    This reverts commit 5f5227bae790eefea3a8ee731de677cee4a54ab5.

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
-
+    
 const Contact = ({listing}) => {
     const [landlord, setLandlord] = useState(null);
     const [message, setMessage] = useState(''); 
